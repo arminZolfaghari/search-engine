0f4df91ad6edae58c1194521adda60a1b8ce9012
arminZolfaghari <arminzd1@gmail.com>
2021-12-21 02:03:08 +0330

    complete champions list

--- Score.py
+++ Score.py
@@ -1,6 +1,9 @@
+import json
+import math
 from PositionalPosting import load_positional_postings_list
 from VectorSpace import load_vectors_list, positional_postings_lists
 
+CHAMPIONS_LIST_FILE = "./champions_list.json"
 POSITIONAL_POSTINGS_LIST_FILE_WITH_STOP_WORDS = "./positional_postings_lists_with_stop_words.json"
 VECTORS_WITHOUT_STOP_WORDS_LIST = "./vectors_without_stop_words.json"
 vectors_list = load_vectors_list(VECTORS_WITHOUT_STOP_WORDS_LIST)
@@ -64,17 +67,43 @@ def index_elimination(query_vector):
         # union of documents list has at least one term of the query
         print(documents_list)
         print(type(documents_list))
-        documents_list_after_index_elimination = list(set(list(documents_list) + documents_list_after_index_elimination))
+        documents_list_after_index_elimination = list(
+            set(list(documents_list) + documents_list_after_index_elimination))
 
     return documents_list_after_index_elimination
 
 
 def create_champions_list():
-    pass
+    """
+    for increase performance, we create champions list for all term in dictionary
+    if term exists more than 10 documents, we save 60% of documents that highest score in champions list
+    else we save all documents in champions list
+    :return: term_champions_dict
+    """
+    term_champions_dict = {}
+    for term, termInfo in positional_postings_lists.items():
+        document_frequency_dict = termInfo["document_frequency_dict"]  # key is document id, value is frequency
+
+        sorted_document_frequency_dict = sort_doc_score_dict(document_frequency_dict)
+        if len(sorted_document_frequency_dict) <= 10:
+            term_champions_dict[term] = list(sorted_document_frequency_dict.keys())
+        else:
+            champion_list_len = math.ceil(len(sorted_document_frequency_dict) * 6 / 10)
+            term_champions_dict[term] = list(sorted_document_frequency_dict.keys())[0: champion_list_len]
+
+    return term_champions_dict
 
 
-def get_champions_list():
-    pass
+def save_champions_list(file_name, term_champions_list):
+    with open(file_name, 'w', encoding='utf-8') as fp:
+        json.dump(term_champions_list, fp, sort_keys=True, indent=4, ensure_ascii=False)
+
+
+def load_champions_list(file_name):
+    with open(file_name, 'r', encoding='utf-8') as fp:
+        term_champions_dict = json.load(fp)
+
+    return term_champions_dict
 
 
 if __name__ == "__main__":
